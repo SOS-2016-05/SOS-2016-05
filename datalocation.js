63092aa91d8a414f8e19ab2e1ac2084f75657f54
Antonio Jiménez Vega <ant.jim.v@gmail.com>
2016-02-27 16:58:19 +0100

    First version of datalocation

---
+++ datalocation.js
@@ -0,0 +1,21 @@
+var express=require("express");
+var fs=require("fs");
+var dat=[];
+
+var app=express();
+
+app.get("/",(req,res)=>{
+  fs.readFile('data.json','utf8',(err,content)=>{
+    console.log("Data read");
+    dat=JSON.parse(content);
+    res.write("<html><body>_____Information_____<ul>");
+    dat.forEach((d)=>{
+      res.write("<li>"+d.country+", "+d.year+", "+d.event+", "+d.edition+"</li>");
+    });
+
+    res.write("</ul>__________________</body></html>");
+    res.end();
+  });
+});
+
+app.listen(16071);
