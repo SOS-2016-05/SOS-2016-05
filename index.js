ad6720d0addb4c6c18bc5731bf6b087844b65050
Antonio Jiménez Vega <ant.jim.v@gmail.com>
2016-02-28 19:57:22 +0100

    gold-medals

--- index.js
+++ index.js
@@ -15,5 +15,21 @@ app.get("/about",(req,res)=>{
   res.end();
 });
 
+
+//Mario
+app.get("/about/gold-medals",function (req,res) {
+  fs.readFile('datagoldmedals.json','utf8',function (err,content) {
+    console.log("Data read");
+    dat=JSON.parse(content);
+    res.write("<html><body>_____Information_____<ul>");
+    dat.forEach(function (d) {
+      res.write("<li>"+d.country+", "+d.year+", "+d.numgoldmedals+", "+d.numsilvermedals+"</li>");
+    });
+
+    res.write("</ul>__________________</body></html>");
+    res.end();
+  });
+});
+
 app.listen(process.env.PORT)
 //app.listen(process.env.PORT); //default port of heroku
