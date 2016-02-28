507703f46ccdcd8b4f0f16e42da5f2b1c23d0b87
Enrique Guerrero Fernández <enriqueguefer@gmail.com>
2016-02-28 19:13:02 +0100

    This version include all the group parts

---
+++ about/athletesnumber.js
@@ -0,0 +1,22 @@
+var express= require("express");
+var fs=require("fs");
+var athletesnumber=[];
+var app=express();
+
+app.get("/",(req,res)=>{
+	fs.readFile('dataatheletesnumber.json','utf8',(err,content)=>{//LEER DE FORMA ASÍNCRONA
+	console.log("Data read");
+	contacts2= JSON.parse(content);
+	
+	res.write("<body><html>Country----------Year----------male athletes number----------female athletes number");
+	contacts2.forEach((athletesnumber)=>{
+	res.write("<br>-"+athletesnumber.country+"-----------"+athletesnumber.year+"--------------------"+
+		athletesnumber.maleathletesnumber+"-------------------------------"+athletesnumber.femaleathletesnumber+"</br>");
+});
+res.write("</ul></body></html>");
+res.end()
+});
+});
+
+gapp.listen(process.env.PORT);
+//app.listen(16071);
