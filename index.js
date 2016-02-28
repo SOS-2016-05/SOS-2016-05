bbd0aded45b2a16fc37a167f4ac048c8221ccf10
Antonio Jiménez Vega <ant.jim.v@gmail.com>
2016-02-28 19:00:20 +0100

    index+location, not found

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app=express();
 app.get("/about",(req,res)=>{
   res.write("<html><body>_____Group Members_____<ul>");
 
-  res.write("<li>Antonio Jimenez Vega:<a href="/about/location.js"> location</a></li>");
+  res.write("<li>Antonio Jimenez Vega: location</li>");
   res.write("<li>Enrique Guerrero Fernandez: participants-number</li>");
   res.write("<li>Mario Esteban Ucles: gold-medals</li>");
 
