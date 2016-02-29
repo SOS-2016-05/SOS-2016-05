877b20e41b26a61eefa3eb313955dcae1858bc5a
Antonio Jiménez Vega <ant.jim.v@gmail.com>
2016-02-29 11:25:49 +0100

    test

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ var app=express();
 var fs=require("fs");   //for all files.
 
 var dat=[];   //data location
+var athletesnumber=[];  //data athletesnumber
 
 app.get("/about",(req,res)=>{
   res.write("<html><body>_____Group Members_____<ul>");
 
   res.write("<li>Antonio Jimenez Vega: <a href='/about/location'>location</a></li>");
-  res.write("<li>Enrique Guerrero Fernandez: participants-number</li>");
-  res.write("<li>Mario Esteban Ucles: gold-medals</li>");
+  res.write("<li>Enrique Guerrero Fernandez:<a href='/about/athletesnumber'> participants-number</a></li>");
+  res.write("<li>Mario Esteban Ucles: <a href='/about/gold-medals>gold-medals</a></li>");
 
   res.write("<ul>________Theme________");
   res.write("<li>Our sources of information are aimed at analyzing the relationship between the location, no of participants and achieved gold medals in the Olympic Games throughout history.</li>");
@@ -39,8 +40,40 @@ app.get("/about/location",(req,res)=>{
 
 
 
-//});
+//gold-medals
+app.get("/about/gold-medals",function (req,res) {
+  fs.readFile('datagoldmedals.json','utf8',function (err,content) {
+    console.log("Data read");
+    dat=JSON.parse(content);
+    res.write("<html><body>_____Information_____<ul>");
+    dat.forEach(function (d) {
+      res.write("<li>"+d.country+", "+d.year+", "+d.numgoldmedals+", "+d.numsilvermedals+"</li>");
+    });
 
+    res.write("</ul>__________________</body></html>");
+    res.end();
+  });
+});
+
+//--------------
+
+//athletesnumber
+app.get("/about/athletesnumber",(req,res)=>{
+	fs.readFile('dataatheletesnumber.json','utf8',(err,content)=>{//LEER DE FORMA ASÍNCRONA
+	console.log("Data read");
+	contacts2= JSON.parse(content);
+
+	res.write("<body><html>Country----------Year----------male athletes number----------female athletes number");
+	contacts2.forEach((athletesnumber)=>{
+	res.write("<br>-"+athletesnumber.country+"-----------"+athletesnumber.year+"--------------------"+
+		athletesnumber.maleathletesnumber+"-------------------------------"+athletesnumber.femaleathletesnumber+"</br>");
+});
+res.write("</ul></body></html>");
+res.end()
+});
+});
+
+//------------
 
 
 app.listen(process.env.PORT);
