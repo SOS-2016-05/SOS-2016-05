4022b2c61ed6fa353ee09e8c14ede9daa98a8fe1
Enrique Guerrero Fernández <enriqueguefer@gmail.com>
2016-03-13 19:07:25 +0100

    The issues #31, #35 and #25 are closed by Enrique

--- index.js
+++ index.js
@@ -197,8 +197,85 @@ app.put("/api/sandbox/musicbands", function (req,res){
 		 res.sendStatus(200);
    });
 
-//---------------------------------------------------------------------------
+//-----------------------SPORTSCENTERS-------------------------------------------
+var sportscenters=[{name: "Ramon_Sanchez_Pizjuan"},
+ {name:"Benito_Villamarin"},
+ {name:"Pabellon_de_deportes_San_pablo"},
+ {name:"Estadio_de_la_Cartuja"}];
 
+  app.get("/api/sandbox/sportscenters",(req,res)=>{   //get list of all Sports Centers
+      console.log("New GET for directory listing");
+    res.status(200).jsonp(sportscenters);
+  });
+
+  app.get("/api/sandbox/sportscenters/:name",(req,res)=>{ //get the name of a sport center
+     var name = req.params.name;
+      console.log("New GET of resource "+name);
+    var sportcenter = StrArray(req.params.name,sportscenters);
+    if(sportscenters != -1){
+      res.send(sportscenters[sportcenter]);
+      res.sendStatus(200);
+    }
+    else{
+      res.sendStatus(404);
+    }
+  });
+
+  app.get('/api-test/sportscenters/loadInitialData',(req,res)=>{  //load json sports centers
+    sportscenters= [];
+    var content=fs.readFileSync('sportscenters.json','utf8');
+    sportscenters = JSON.parse(content);
+    res.sendStatus(200);
+  });
+
+  app.post("/api/sandbox/sportscenters",(req,res)=>{  //post ****
+        var spcenter = req.body;
+        sportscenters.push(spcenter);
+        console.log("New POST of resource "+spcenter.name);
+        res.sendStatus(200);
+    });
+
+    app.post("/api/sandbox/sportscenters/:name",(req,res)=>{    //post FORBIDDEN
+        res.send("Error: Forbidden action, the POST method must be do over a directory");
+        res.sendStatus(400);
+    });
+
+   app.put('/api/sandbox/sportscenters/:name',(req, res)=>{ //put
+        var temp2 = req.body;
+        var id2 = req.params.name;
+        var sportcenter = StrArray(id2,sportscenters);
+        if (sportcenter != -1){
+            sportscenters[sportcenter].name=temp2.name;
+            res.send(200);
+      }
+      else{
+          res.send(404);
+      }
+    });
+
+    app.put("/api/sandbox/sportscenters",(req,res)=>{ //put FORBBIDEN
+        res.send("Error: Forbidden action, the POST method must not be do over a directory");
+        res.sendStatus(400);
+    });
+
+    app.delete("/api/sandbox/sportscenters/:name",(req,res)=>{  //delete name
+       var name=req.params.name;
+       console.log("New DELETE of resource "+name);
+       var sportcenter = StrArray(name,sportscenters);
+       if (sportcenter != -1){
+         sportscenters.splice(sportcenter,1);
+         res.sendStatus(200);
+       }
+       else{
+        res.sendStatus(404);
+       }
+     });
+
+   app.delete("/api/sandbox/sportscenters",(req,res)=>{  //delete list
+       console.log("New DELETE of all resources");
+       sportscenters.splice(0,sportscenters.length);
+       res.sendStatus(200);
+     });
 
 /*
 //location
