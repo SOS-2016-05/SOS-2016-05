147714fc8db0f95ba349027d689ab496eb7e4396
Mario Esteban Uclés <mario.esteban@posteo.es>
2016-04-09 18:49:40 +0200

    Some fixes

--- data/gold-medals/gold-medals.js
+++ data/gold-medals/gold-medals.js
@@ -1,34 +1,26 @@
-var service = '../../api/v1/';
-
 $(document).ready(function(){
-
-    jQuery.support.cors = true;
-
     $.ajax({
         type: "GET",
-        url: service + 'gold-medals?apikey=sosrw',
-        data: "{}",
+        url: '/api/v1/gold-medals?apikey=sosrw',
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        cache: false,
         success: function(data) {
-        drawTable(data);
-        var trHTML = '';
+            insertTable(data);
     	}
     });
-    function drawTable(data) {
-	    for (var i = 0; i < data.length; i++) {
-	        drawRow(data[i]);
-	    }
-	}	
-
-	function drawRow(rowData) {
-	    var row = $("<tr bgcolor='#FFFFFF'/>")
-	    $("#location").append(row); 
-	    row.append($("<td>" + rowData.country + "</td>"));
-	    row.append($("<td>" + rowData.year + "</td>"));
-	    row.append($("<td>" + rowData.goldmedalsnumber + "</td>"));
-	    row.append($("<td>" + rowData.silvermedalsnumber + "</td>"));
+    	
+	function insertRow(data) {
+	    var row = $("<tr/>")
+	    $("#medals").append(row); 
+	    row.append($("<td>"+data.country+"</td>"));
+	    row.append($("<td>"+data.year+"</td>"));
+	    row.append($("<td>"+data.goldmedalsnumber+"</td>"));
+	    row.append($("<td>"+data.silvermedalsnumber+"</td>"));
+	}
+    
+    function insertTable(data) {
+	    for (var i = 0; i < data.length; i++)
+	        insertRow(data[i]);
 	}
 
 })
