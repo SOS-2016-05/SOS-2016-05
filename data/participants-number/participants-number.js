9cf3ed1077e07a321618300e1f5889eb777075e6
Enrique Guerrero Fernández <enriqueguefer@gmail.com>
2016-04-12 16:25:48 +0200

    The issues #65 and #62 are completed

---
+++ data/participants-number/participants-number.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        url: '/api/v1/participants-number?apikey=abc',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data) {
+            insertTable(data);
+    	}
+    });
+    	
+	function insertRow(data) {
+	    var row = $("<tr/>")
+	    $("#participantsnumber").append(row); 
+	    row.append($("<td>"+data.country+"</td>"));
+	    row.append($("<td>"+data.year+"</td>"));
+	    row.append($("<td>"+data.maleathletesnumber+"</td>"));
+	    row.append($("<td>"+data.femaleathletesnumber+"</td>"));
+	}
+    
+    function insertTable(data) {
+	    for (var i = 0; i < data.length; i++)
+	        insertRow(data[i]);
+	}
+
+})
