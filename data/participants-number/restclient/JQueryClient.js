75d448b7c99a1a130426dfa94f82bd0e1cd29576
Enrique Guerrero Fernández <enriqueguefer@gmail.com>
2016-04-24 23:20:57 +0200

    The issue #78 is finished

---
+++ data/participants-number/restclient/JQueryClient.js
@@ -0,0 +1,38 @@
+$(document).ready(() => {
+    console.log("Jquery ready!");
+
+    $("#button").click(() => {
+        console.log("Handling click");
+        var method = $("input[type=radio]:checked").attr("id");
+
+        var request = $.ajax({
+            url:$("#url").val(),
+            type: method,
+            data:$("#payload").val(),
+            contentType: "application/json"
+        });
+
+        request.done(function(data,status,jqXHR) {
+            console.log("Handling request (OK)");
+            console.log("Data received:");
+            var jsonString = JSON.stringify(data);
+            console.log(jsonString);
+            $("#data").html(jsonString);
+            $("#status").html(jqXHR.status);
+            $("#log").html(status);
+
+        });
+
+        request.always(function (jqXHR,status){
+            if(status=="error"){
+                console.log("Status: "+jqXHR.status);
+                $("#data").html(" ");
+                $("#status").html(jqXHR.status);
+                $("#log").html(status);
+            }
+                
+        });
+            
+
+    });
+});
